ce31934424e6d38e5e8d27ddac8d09a6f9293a12
alisndb <omarovali6@mail.ru>
2022-07-08 22:52:29 +0300

    fix: wrong code logic

--- select.py
+++ select.py
@@ -11,7 +11,7 @@ SELECT g.name, COUNT(ga.author_id) FROM genre g
     GROUP BY g.id;
 """
 res = connection.execute(code).fetchall()
-print(f'Кол-во исполнителей по жанрам: {res}')
+print(f'1. Кол-во исполнителей по жанрам: {res}')
 
 
 code = """
@@ -21,7 +21,7 @@ SELECT COUNT(t.id) FROM album a
     GROUP BY t.id;
 """
 res = len(connection.execute(code).fetchall())
-print(f'Кол-во треков, вошедших в альбомы 2019-2020 гг.: {res}')
+print(f'2. Кол-во треков, вошедших в альбомы 2019-2020 гг.: {res}')
 
 
 code = """
@@ -30,32 +30,33 @@ SELECT a.name, AVG(t.length) FROM album a
     GROUP BY a.id;
 """
 res = connection.execute(code).fetchall()
-print(f'Средняя продолжительность треков по каждому альбому (сек.): {[(i[0], float(i[1])) for i in res]}')
+print(f'3. Средняя продолжительность треков по каждому альбому (сек.): {[(i[0], float(i[1])) for i in res]}')
 
 
 code = """
-SELECT au.name FROM album al
+SELECT DISTINCT au.name FROM album al
     JOIN albumauthor aa ON al.id = aa.album_id
     JOIN author au ON aa.author_id = au.id
-    WHERE al.year != 2020
-    GROUP BY au.id;
+    WHERE au.id NOT IN (SELECT DISTINCT au.id FROM album al
+                            JOIN albumauthor aa ON al.id = aa.album_id
+                            JOIN author au ON aa.author_id = au.id
+                            WHERE al.year = 2020);
 """
 res = connection.execute(code).fetchall()
-print(f'Исполнители, не выпустившие альбомы в 2020 г.: {[i[0] for i in res]}')
+print(f'4. Исполнители, не выпустившие альбомы в 2020 г.: {[i[0] for i in res]}')
 
 
 code = """
-SELECT c.name FROM collection c
+SELECT DISTINCT c.name FROM collection c
     JOIN collectiontrack ct ON c.id = ct.collection_id
     JOIN track t ON ct.track_id = t.id
     JOIN album a ON t.album_id = a.id
     JOIN albumauthor aa ON a.id = aa.album_id
     JOIN author au ON aa.author_id = au.id
     WHERE au.name LIKE 'Louis Armstrong'
-    GROUP BY c.id;
 """
 res = connection.execute(code).fetchall()
-print(f'Сборники, в которых присутствует исполнитель Louis Armstrong: {[i[0] for i in res]}')
+print(f'5. Сборники, в которых присутствует исполнитель Louis Armstrong: {[i[0] for i in res]}')
 
 
 code = """
@@ -67,7 +68,7 @@ SELECT a.name FROM album a
     HAVING COUNT(ga.genre_id) > 1;
 """
 res = connection.execute(code).fetchall()
-print(f'Альбомы, в которых есть исполнители более 1-го жанра: {"NULL" if len(res) == 0 else [i[0] for i in res]}')
+print(f'6. Альбомы, в которых есть исполнители более 1-го жанра: {"NULL" if len(res) == 0 else [i[0] for i in res]}')
 
 
 code = """
@@ -77,26 +78,29 @@ SELECT t.name FROM track t
     WHERE c.id IS NULL;
 """
 res = connection.execute(code).fetchall()
-print(f'Треки, которые не входят в сборники: {[i[0] for i in res]}')
+print(f'7. Треки, которые не входят в сборники: {[i[0] for i in res]}')
 
 
 code = """
-SELECT au.name FROM track t
+SELECT DISTINCT au.name FROM track t
     JOIN album a ON t.album_id = a.id
     JOIN albumauthor aa ON a.id = aa.album_id
     JOIN author au ON aa.author_id = au.id
     WHERE t.length = (SELECT MIN(length) FROM track)
-    GROUP BY au.id
 """
 res = connection.execute(code).fetchall()
-print(f'Исполнители, написавшие самый короткий по продолжительности трек: {[i[0] for i in res]}')
+print(f'8. Исполнители, написавшие самый короткий по продолжительности трек: {[i[0] for i in res]}')
 
 
 code = """
-SELECT a.name, COUNT(t.id) FROM album a
+SELECT a.name FROM album a
     JOIN track t ON a.id = t.album_id
-    GROUP BY a.id;
+    GROUP BY a.id
+    HAVING COUNT(t.id) = (SELECT COUNT(t.id) FROM album a
+                            JOIN track t ON a.id = t.album_id
+                            GROUP BY a.id
+                            ORDER BY COUNT(t.id)
+                            LIMIT 1);
 """
 res = connection.execute(code).fetchall()
-min_val = min([i[1] for i in res])
-print(f'Альбомы с наименьшим кол-вом треков: {[i[0] for i in res if i[1] == min_val]}')
+print(f'9. Альбомы с наименьшим кол-вом треков: {[i[0] for i in res]}')
